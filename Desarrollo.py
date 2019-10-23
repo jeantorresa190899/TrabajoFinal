27af475356fe5e854ca286f3186ac2cc8a2e4cb4
DAVZ2909 <diegoandre09@hotmail.com>
2019-10-23 08:43:11 -0500

    puse el intro

--- Desarrollo.py
+++ Desarrollo.py
@@ -2,7 +2,14 @@ import os, sys
 os.system("cls")
 
 
-import random
-num = random.randint(0,10)
+# Inicio de la Aplicación
 
-print(num)
+print("Hola, bienvenido a esta nueva plataforma de aprendizaje.")
+print("¿Cual es su nombre?")
+nombre = str(input("Ingrese su nombre: "))
+
+print("hola", nombre)
+print(nombre, ", este programa consiste en responder 20 preguntas de cultura" 
++ " general, \n las cuales te ayudarán a ")
+
+
