e94c42a3a1f1b6b6e6a56bd7ad7149e8f88d4b55
Andre Annunziata <andre.annunziata@usil.pe>
2019-10-29 08:25:34 -0500

    Desarrollo

--- Desarrollo.py
+++ Desarrollo.py
@@ -12,18 +12,33 @@ print("Hola", nombre)
 print("Este programa consiste en responder 20 preguntas de cultura" 
 + " general, \n las cuales te ayudarán a expandir tus conociminetos.")
 
-respuesta = str(input("Comenzamos? "))
+comenzar = str(input("Comenzamos? "))
 
-print("1. ¿Cuál es un tipo de sabor primario?")
-print("a) quemado")
-print("b) umami")
-print("c) rostizado")
-print ("d) sabroso")
-respuesta1 = str(input(""))
-if respuesta1 == "umami" or "Umami" or "b" or "B":
-    print("Correcto!")
-else:
-    print("Incorrecto")
+
+class Question:
+     def __init__(self, prompt, answer):
+          self.prompt = prompt
+          self.answer = answer
+
+question_prompts = [
+     "¿Cuál es un tipo de sabor primario?\n(a) Quemado\n(b)Rostizado\n(c)Umami\n(d)Sabroso\n\n",
+     "What color are bananas?\n(a) Red/Green\n(b)Yellow\n\n",
+]
+
+questions = [
+     Question(question_prompts[0], "c"),
+     Question(question_prompts[1], "b"),
+]
+
+def run_quiz(questions):
+     score = 0
+     for question in questions:
+          answer = input(question.prompt)
+          if answer == question.answer:
+               score += 1
+     print("you got", score, "out of", len(questions))
+
+run_quiz(questions)
 
 
 
