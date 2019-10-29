f41b254a58b7347fe7ac339e7e3444247e118260
Andre Annunziata <andre.annunziata@usil.pe>
2019-10-29 07:56:56 -0500

    Desarrollo

--- Desarrollo.py
+++ Desarrollo.py
@@ -12,4 +12,18 @@ print("Hola", nombre)
 print("Este programa consiste en responder 20 preguntas de cultura" 
 + " general, \n las cuales te ayudarán a expandir tus conociminetos.")
 
+respuesta = str(input("Comenzamos? "))
+
+print("1. ¿Cuál es un tipo de sabor primario?")
+print("a) quemado")
+print("b) umami")
+print("c) rostizado")
+print ("d) sabroso")
+respuesta1 = str(input(""))
+if respuesta1 == "umami":
+    print("Correcto!")
+else:
+    print("Incorrecto")
+
+
 
