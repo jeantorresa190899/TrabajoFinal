e66d810395f0894c7f9c4cae14bf7d45b7281b3d
David <david.huetado2000@gmail.com>
2019-11-05 07:40:56 -0500

    TrabajoFinal

--- Desarrollo.py
+++ Desarrollo.py
@@ -11,7 +11,7 @@ nombre = str(input("¿Cuál es tu nombre? -----------> "))
 
 print("Hola", nombre)
 print("Este programa consiste en responder 20 preguntas de cultura" 
-+ " general, \nlas cuales te ayudarán a expandir tus conociminetos.")
++ " general, \nlas cuales te ayudarán a expandir tus conocimientos.")
 print("----------------------------------------------------------------")
 print("----------------------------------------------------------------")
 print("¿Estas preparado?")
@@ -32,7 +32,15 @@ question_prompts = [
      "¿En qué continente está San Marino?\na) América del Norte\nb)América del Sur\nc)Europa\nd)Asia\n\n",
      "¿Qué cantidad de huesos hay en el cuerpo humano?\na) 212\nb)206\nc)300\nd)120\n\n",
      "¿En qué país se encuentra la torre de Pisa?\na) Alemania\nb)España\nc)Italia\nd)Suiza\n\n",
-     "¿Qúe año llegó Cristobal Colón a América?\na) 1592\nb)1492\nc)1692\nd)1392\n\n",
+     "¿Qúe año llegó Cristóbal Colón a América?\na) 1592\nb)1492\nc)1692\nd)1392\n\n",
+     "¿Qué deporte practicaba Michael Jordan?\na) Baseball\nb)Football\nc)Basketball\nd)Golf\n\n",
+     "¿Cual es país más poblado de la Tierra?\na) India\nb)China\nc)Estados Unidos de América\nd)Brasil\n\n",
+     "¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?\na) Rómanico\nb)Barroco\nc)Neoclásico\nd)Gótico\n\n",
+     "¿Quién va a la cárcel?\na) Imputado\nb)Acusado\nc)Condenado\nd)Testigo\n\n",
+     "¿A qué país pertenece la ciudad de Varsovia?\na) Polonia\nb)Austria\nc)Rusia\nd)Bielorusia\n\n",
+     "¿Cuál es el metal más caro del mundo?\na) Oro\nb)Plata\nc)Rodio\nd)Aluminio\n\n",
+     "¿Cuál es la nacionalidad de Pablo Neruda?\na) Chilena\nb)Boliviana\nc)Argentina\nd)Uruguaya\n\n",
+
 
 
 
@@ -65,21 +73,21 @@ question_prompts = [
      "\na) \nb) \nc) \nd)\n\n"
      "\na) \nb) \nc) \nd)\n\n"
      "\na) \nb) \nc) \nd)\n\n"
-     "¿ Cuantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n\n",
-     "¿ Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n\n",
-     "¿ Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n\n",
-     "¿ Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n\n",
-     "¿ Cuántas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n\n",
-     "¿ Cuantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n\n",
-     "¿ Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n\n",
-     " La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n\n",
-     " Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n\n",
-     " ¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n\n",
-     " Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n\n",
-     " Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n\n",
-     " La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n\n",
-     " La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n\n",
-     " ¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n\n",
+     "¿ Cuantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n",
+     "¿ Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n",
+     "¿ Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n",
+     "¿ Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n",
+     "¿ Cuántas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n",
+     "¿ Cuantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n",
+     "¿ Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n",
+     " La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n",
+     " Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n",
+     " ¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n",
+     " Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n",
+     " Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n",
+     " La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n",
+     " La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n",
+     " ¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n",
      ]
 
 questions = [
