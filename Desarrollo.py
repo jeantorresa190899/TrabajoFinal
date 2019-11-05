7a9c8520b67329e80d48d6818469c35f3405511e
jeantorresa190899 <jeantorresa190899@gmail.com>
2019-11-05 08:02:05 -0500

    addddddddd

--- Desarrollo.py
+++ Desarrollo.py
@@ -28,13 +28,13 @@ question_prompts = [
      "¿Cuál es un tipo de sabor primario?\na) Quemado\nb)Rostizado\nc)Umami\nd)Sabroso\n\n",
      "¿Cuál es el lugar más frío de la tierra??\n(a) Antartida\n(b)Suecia\n(c)Groenlandia\n(d)Islandia\n\n",
      "¿Quién escribió La Odisea?\na) Sócrates\nb)Pitágoras\nc)Homero\nd)Aristóteles\n\n",
-     "¿Cuántos Estados tiene integrados Estados Unidos? \na) 32\nb)49\nc)50\nd)55\n\n",
+     
      "¿En qué continente está San Marino?\na) América del Norte\nb)América del Sur\nc)Europa\nd)Asia\n\n",
-     "¿Quién inventó el primer avión?\na) Los hermanos Wright\nb)Los hermanos Warner\nc)Los hermanos Wachowski\nd)Los hermanos Lumiére\n\n",
-     "¿Quién escribió Cien años de soledad?\na) Gabriel García Márquez\nb)Alfredo Bryce Echenique\nc)Cesar Vallejo\nd)Ricardo Úceda\n\n",
-     "¿En qué deporte destacaba Toni Elías?\na) Motociclismo\nb)Fútbol\nc)Formula 1\nd)Voley\n\n",
+     
+    
+     
      "¿Qué deporte practicaba Michael Jordan?\na) Baseball\nb)Football\nc)Basketball\nd)Golf\n\n",
-     "¿Dónde se inventó el ping-pong?\na) Estados Unidos de América\nb)Inglaterra\nc)Canadá\nd)Irlanda\n\n",
+     
      "¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?\na) Rómanico\nb)Barroco\nc)Neoclásico\nd)Gótico\n\n",
      "¿Quién va a la cárcel?\na) Imputado\nb)Acusado\nc)Condenado\nd)Testigo\n\n",
      "¿A qué país pertenece la ciudad de Varsovia?\na) Polonia\nb)Austria\nc)Rusia\nd)Bielorusia\n\n",
@@ -55,21 +55,21 @@ question_prompts = [
      "¿Cuantos corazones tienen los pulpos?\na)2 \nb)1 \nc)3 \nd)5\n\n"
      "¿Cuál es el libro sagrado del Islam?\na)Biblia \nb)Coran \nc)Credo\nd)Documento de Damasco\n\n"
      "¿En qué país se ubica la Casa Rosada?\na)EE.UU \nb)Uruguay \nc)Argentina \nd)Chile\n\n"
-     "¿Cúantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n",
-     "¿Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n",
-     "¿Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n",
-     "¿Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n",
-     "¿Cúantas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n",
-     "¿Cúantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n",
-     "¿Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n",
-     "La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n",
-     "Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n",
-     "¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n",
-     "Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n",
-     "Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n",
-     "La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n",
-     "La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n",
-     "¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n",
+     "¿ Cuantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n",
+     "¿ Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n",
+     "¿ Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n",
+     "¿ Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n",
+     "¿ Cuántas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n",
+     "¿ Cuantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n",
+     "¿ Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n",
+     " La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n",
+     " Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n",
+     " ¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n",
+     " Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n",
+     " Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n",
+     " La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n",
+     " La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n",
+     " ¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n",
      ]
 
 questions = [
@@ -113,11 +113,11 @@ questions = [
      Question(question_prompts[37], "d"),
      Question(question_prompts[38], "c"),
      Question(question_prompts[39], "c"),
-     Question(question_prompts[41], "b"),
+     Question(question_prompts[40], "b"),
+     Question(question_prompts[41], "d"),
      Question(question_prompts[42], "d"),
-     Question(question_prompts[43], "d"),
-     Question(question_prompts[44], "b"),
-     Question(question_prompts[45], "a")
+     Question(question_prompts[43], "b"),
+     Question(question_prompts[44], "a")
      
 ]
 
