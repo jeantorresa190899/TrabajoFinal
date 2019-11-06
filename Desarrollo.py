1f494c9d7d2746299925c175fb568e2ec883050f
jeantorresa190899 <jeantorresa190899@gmail.com>
2019-11-06 08:30:09 -0500

    adddddcuack

--- Desarrollo.py
+++ Desarrollo.py
@@ -41,7 +41,7 @@ question_prompts = [
      "¿Cuál es el metal más caro del mundo?\na)Oro\nb)Plata\nc)Rodio\nd)Aluminio\n\n",
      "¿Cuál es la nacionalidad de Pablo Neruda?\na)Chilena\nb)Boliviana\nc)Argentina\nd)Uruguaya\n\n",
      "¿Cuál es el país más poblado del mundo?\na)Rusia\nb)China\nc)EE.UU\nd)Canadá\n\n",
-     "¿Quién fue el líder de los nazis durante la Segunda Guerra Mundial? \na)Mussolini \nb)Stalin \n c)Hitler \nd)F.Roosevelt\n\n",
+     "¿Quién fue el líder de los nazis durante la Segunda Guerra Mundial? \na)Mussolini \nb)Stalin \nc)Hitler \nd)F.Roosevelt\n\n",
      "¿En qué país se encuentra la torre de Pisa? \na)Italia \nb)Francia \nc)España \nd)Alemania \n\n",
      "¿Cuantos huesos tiene el cuerpo humano? \na)214 \nb)206 \nc)216 \nd)202 \n\n",
      "¿Cual de los siguientes animales es un marsupial? \na)Gato \nb)Koala \nc)Chimpancé \nd)Conejo\n\n",
@@ -55,21 +55,21 @@ question_prompts = [
      "¿Cuantos corazones tienen los pulpos?\na)2 \nb)1 \nc)3 \nd)5\n\n",
      "¿Cuál es el libro sagrado del Islam?\na)Biblia \nb)Coran \nc)Credo\nd)Documento de Damasco\n\n",
      "¿En qué país se ubica la Casa Rosada?\na)EE.UU \nb)Uruguay \nc)Argentina \nd)Chile\n\n",
-     "¿ Cuantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n\n",
-     "¿ Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n\n",
-     "¿ Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n\n",
-     "¿ Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n\n",
-     "¿ Cuántas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n\n",
-     "¿ Cuantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n\n",
-     "¿ Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n\n",
-     " La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n\n",
-     " Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n\n",
-     " ¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n\n",
-     " Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n\n",
-     " Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n\n",
-     " La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n\n",
-     " La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n\n",
-     " ¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n\n",
+     "¿Cuantas fueron las principales cruzadas(1095 - 1291)?\na)3 \nb)6 \nc)8 \nd)5\n\n",
+     "¿Quién fue el primer presidente del Perú?\na)Don José de San Martín \nb)José Mariano de la Riva Agüero y Sánchez Boquete \nc)José Bernardo de Torre Tagle \nd)José de la Mar\n\n",
+     "¿Cómo se la nombró a la primera computadora programable ?\na)Maquina de turing \nb)Z1 \nc)Eniac \nd)Osborne\n\n",
+     "¿Cuál ha sido la guerra más sangrienta de la historia?\na)1ra guerra mundial \nb)2da guerra mubdial \nc)Guerra de vietnam \nd)Guerra civil española\n\n",
+     "¿Cuántas patas tiene una abeja?\na)6 \nb)10 \nc)4 \nd)8\n\n",
+     "¿Cuantos años tiene un lustro ?\na)5 \nb)10 \nc)25 \nd)50\n\n",
+     "¿Con qué otro nombre se denomina al hexaedro?\na)cono \nb)piramide \nc)esfera \nd)cubo\n\n",
+     "La capital de Irlanda es:\na)Budapest \nb)Berlín \nc)Atenas \nd)Dúblin\n\n",
+     "Si el radio de un círculo mide 5 centímetros, ¿cuánto mide el diámetro?\na)5 centímetros \nb)20 centímetros \nc)10 centímetros \nd)2 centímetros\n\n",
+     "¿Cuál es el planeta de mayor tamaño del Sistema Solar?\na)Mercurio \nb)Marte \nc)Júpiter \nd)Tierra\n\n",
+     "Una carga positiva y otra negativa:\na)No pasa nada \nb)Se atraen \nc)Intercambian sus polos \nd)Se repelen\n\n",
+     "Colón se embarcó en su viaje a América en tres embarcaciones, ¿Cuál no fue una de ellas?\na)Pinta \nb)Santa María \nc)La Niña \nd)Santa Cristina\n\n",
+     "La unidad de volumen en el Sistema Internacional es:\na)Amperio por metro \nb)Amperio por metro cuadrado \nc)Metro cuadrado \nd)Metro cúbico\n\n",
+     "La temperatura a la cual la materia pasa de estado líquido a estado gaseoso se denomina:\na)Ecuación de estado \nb)Punto de ebullición \nc)Transición de fase \nd)Punto de fusión\n\n",
+     "¿Cuántas vueltas da el segundero en una vuelta completa en un reloj de doce horas?\na)720 \nb)800 \nc)420 \nd)360\n\n",
      ]
 
 questions = [
@@ -131,17 +131,18 @@ def run_quiz(questions):
           print("................")
           print("Pregunta:", i+1)
           answer = input(question.prompt)
+          
           i +=1
           if answer == question.answer:
                score += 1      
      print("Tienes", score, "de", len(questions))
+     
 
-
-     if score <= 11:
+     if score <= 5:
           mensaje = "- Vuelvo a intentar"
-     elif score <=20:
+     elif score <=10:
           mensaje = "- Not bad!"
-     elif score <=30:
+     elif score <=15:
           mensaje = "- Buen intento!"
      else:
           mensaje = "- Buen trabajo!"
